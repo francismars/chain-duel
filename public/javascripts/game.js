09b310c1b3064c3b8aab0382bd4158b68f6e28be
Pedro <pedromvprg@gmail.com>
2023-03-27 03:30:13 +0100

    Center intro text

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -279,28 +279,28 @@ function displayGame(){
             alturaNumbers = (alturaGame*0.50).toString();
             ctxGame.font = alturaNumbers.concat("px BureauGrotesque");
             if(counterStart==0){
-                ctxGame.fillText("3", (larguraGame*0.25), alturaGame/2);
-                ctxGame.strokeText("2", (larguraGame*0.38), alturaGame/2);
-                ctxGame.strokeText("1", (larguraGame*0.49), alturaGame/2);
-                ctxGame.strokeText("LFG", (larguraGame*0.71), alturaGame/2);
+                ctxGame.fillText("3", (larguraGame*0.22), alturaGame/2);
+                ctxGame.strokeText("2", (larguraGame*0.34), alturaGame/2);
+                ctxGame.strokeText("1", (larguraGame*0.45), alturaGame/2);
+                ctxGame.strokeText("LFG", (larguraGame*0.67), alturaGame/2);
             }
             else if (counterStart==1){
-                ctxGame.fillText("3", (larguraGame*0.25), alturaGame/2);
-                ctxGame.fillText("2", (larguraGame*0.38), alturaGame/2);
-                ctxGame.strokeText("1", (larguraGame*0.49), alturaGame/2);
-                ctxGame.strokeText("LFG", (larguraGame*0.71), alturaGame/2);
+                ctxGame.fillText("3", (larguraGame*0.22), alturaGame/2);
+                ctxGame.fillText("2", (larguraGame*0.34), alturaGame/2);
+                ctxGame.strokeText("1", (larguraGame*0.45), alturaGame/2);
+                ctxGame.strokeText("LFG", (larguraGame*0.67), alturaGame/2);
             }
             else if (counterStart==2){
-                ctxGame.fillText("3", (larguraGame*0.25), alturaGame/2);
-                ctxGame.fillText("2", (larguraGame*0.38), alturaGame/2);
+                ctxGame.fillText("3", (larguraGame*0.22), alturaGame/2);
+                ctxGame.fillText("2", (larguraGame*0.34), alturaGame/2);
                 ctxGame.fillText("1", (larguraGame*0.49), alturaGame/2);
-                ctxGame.strokeText("LFG", (larguraGame*0.71), alturaGame/2);
+                ctxGame.strokeText("LFG", (larguraGame*0.67), alturaGame/2);
             }
             else if (counterStart==3){
-                ctxGame.fillText("3", (larguraGame*0.25), alturaGame/2);
-                ctxGame.fillText("2", (larguraGame*0.38), alturaGame/2);
-                ctxGame.fillText("1", (larguraGame*0.49), alturaGame/2);
-                ctxGame.fillText("LFG", (larguraGame*0.71), alturaGame/2);
+                ctxGame.fillText("3", (larguraGame*0.22), alturaGame/2);
+                ctxGame.fillText("2", (larguraGame*0.34), alturaGame/2);
+                ctxGame.fillText("1", (larguraGame*0.45), alturaGame/2);
+                ctxGame.fillText("LFG", (larguraGame*0.67), alturaGame/2);
             }
             else if (counterStart==4){
                 gameStarted = true;
