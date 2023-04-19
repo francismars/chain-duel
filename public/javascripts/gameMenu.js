f052426636d4d5b83c6fe53eafcb6b6dae784aa5
tantra <tantra@tantra.pt>
2023-04-19 16:47:05 +0100

    changed lnurlW amount to account for designer fee

--- public/javascripts/gameMenu.js
+++ public/javascripts/gameMenu.js
@@ -29,7 +29,7 @@ addEventListener("keydown", function(event) {
                             sessionStorage.setItem("P2Sats", playersSats[1]);
                             sessionStorage.setItem("P1Name", p1Name);
                             sessionStorage.setItem("P2Name", p2Name);
-                            socket.emit('createWithdrawal', Math.floor((playersSats[0]+playersSats[1])*0.98));
+                            socket.emit('createWithdrawal', Math.floor((playersSats[0]+playersSats[1])*0.975));
                             numberofCreates=1;
                         }
                     }  
