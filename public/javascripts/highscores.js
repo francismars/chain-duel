4ea4119e961e2872d7bc2932e11b695427e6e9e9
tantra <tantra@tantra.pt>
2023-04-21 03:30:41 +0100

    improving highscores logic and css

--- public/javascripts/highscores.js
+++ public/javascripts/highscores.js
@@ -11,77 +11,107 @@ fetch('./files/highscores.json')
 
         for(i=0;i<orderedScores.length;i++){
             // "p1Name":"SELLIX","p1sats":1000000,"p2Name":"Pedro","p2sats":1000000,"winner":"Player 1","prize":1960000
-
+                        
             // Highscore Rank
             const elRank = document.createElement("h2");
             const ranktext = document.createTextNode(i+1);
             elRank.appendChild(ranktext);
             elRank.classList.add("rankStyle");
 
-            // P1 Name
-            const elP1Name = document.createElement("h2");
-            const nameP1text = document.createTextNode(highscores[i].p1Name);
-            elP1Name.appendChild(nameP1text);
-            elP1Name.classList.add("p1NameStyle");
+            var winnerName, winnerSats, loserName, loserSats;
+            if(highscores[i].winner=="Player 1"){
+              winnerName = highscores[i].p1Name
+              winnerSats = highscores[i].p1sats
+              loserName = highscores[i].p2Name
+              loserSats = highscores[i].p2sats
+            }
+            else if(highscores[i].winner=="Player 2"){
+              loserName = highscores[i].p1Name
+              loserSats = highscores[i].p1sats
+              winnerName = highscores[i].p2Name
+              winnerSats = highscores[i].p2sats              
+            }
 
-            // P1 Sats
-            const elP1Sats = document.createElement("h2");
-            const p1Satstext = document.createTextNode(highscores[i].p1sats);
-            elP1Sats.appendChild(p1Satstext);
-            elP1Sats.classList.add("p1SatsStyle");
+            // Winner Name
+            const elWinnerName = document.createElement("h2");
+            const winnerP1text = document.createTextNode(winnerName);
+            elWinnerName.appendChild(winnerP1text);
+            elWinnerName.classList.add("winnerNameStyle");
+            
+            // Winner Sats
+            const elWinnerSats = document.createElement("h2");
+            const winnerSatstext = document.createTextNode(winnerSats);
+            elWinnerSats.appendChild(winnerSatstext);
+            elWinnerSats.classList.add("winnerSatsStyle");
 
-            // P1 Sats Text
-            const elSatsP1Label = document.createElement("span");
-            elSatsP1Label.textContent="sats";
-            elP1Sats.appendChild(elSatsP1Label);
-            elSatsP1Label.classList.add("satsP1LabelStyle");
+            // Winner Sats Text
+            const elSatsWinnerLabel = document.createElement("span");
+            elSatsWinnerLabel.textContent="sats";
+            elWinnerSats.appendChild(elSatsWinnerLabel);
+            elSatsWinnerLabel.classList.add("satsWinnerLabelStyle");
+
+            // Winner Infos Div
+            const winnerDivElement = document.createElement('div');
+            winnerDivElement.classList.add("winnerInfo");
+            winnerDivElement.appendChild(elWinnerName);
+            winnerDivElement.appendChild(elWinnerSats);
 
             // VS Text
             const elVSLabel = document.createElement("span");
             elVSLabel.textContent="VS";
             elVSLabel.classList.add("VSLabelStyle");
             
-            // P2 Name
-            const elP2Name = document.createElement("h2");
-            const nameP2text = document.createTextNode(highscores[i].p2Name);
-            elP2Name.appendChild(nameP2text);
-            elP2Name.classList.add("p2NameStyle");
+            // Loser Name
+            const elLoserName = document.createElement("h2");
+            const nameLosertext = document.createTextNode(loserName);
+            elLoserName.appendChild(nameLosertext);
+            elLoserName.classList.add("loserNameStyle");
 
-            // P2 Sats
-            const elP2Sats = document.createElement("h2");
-            const p2Satstext = document.createTextNode(highscores[i].p2sats);
-            elP2Sats.appendChild(p2Satstext);
-            elP2Sats.classList.add("p2SatsStyle");
+            // Loser Sats
+            const elLoserSats = document.createElement("h2");
+            const loserSatstext = document.createTextNode(loserSats);
+            elLoserSats.appendChild(loserSatstext);
+            elLoserSats.classList.add("loserSatsStyle");
 
-            // P1 Sats Text
-            const elSatsP2Label = document.createElement("span");
-            elSatsP2Label.textContent="sats";
-            elP2Sats.appendChild(elSatsP2Label);
-            elSatsP2Label.classList.add("satsP2LabelStyle");
+            // Loser Sats Text
+            const elSatsLoserLabel = document.createElement("span");
+            elSatsLoserLabel.textContent="sats";
+            elLoserSats.appendChild(elSatsLoserLabel);
+            elSatsLoserLabel.classList.add("satsLoserLabelStyle");
+ 
+            // Loser Infos Div
+            const loserdivElement = document.createElement('div');
+            loserdivElement.classList.add("loserinfo");
+            loserdivElement.appendChild(elLoserName);
+            loserdivElement.appendChild(elLoserSats);
 
             // Total Prize
-            const elSats = document.createElement("h2");
+            const elPrize = document.createElement("h2");
             const satsvalue = document.createTextNode(highscores[i].prize);
-            elSats.appendChild(satsvalue);
+            elPrize.classList.add("prizeSatsStyle");
+            elPrize.appendChild(satsvalue);
 
             // Prize Sats
-            const elSatsLabel = document.createElement("span");
-            elSatsLabel.textContent="sats";
-            elSats.appendChild(elSatsLabel);
-            elSats.classList.add("satsLabelStyle");
+            const elPrizeSatsLabel = document.createElement("span");
+            elPrizeSatsLabel.textContent="sats";
+            elPrizeSatsLabel.classList.add("satsLabelStyle");
+
+            // Prize Infos Div
+            const prizedivElement = document.createElement('div');
+            prizedivElement.classList.add("prizeinfo");
+            prizedivElement.appendChild(elPrize);
+            prizedivElement.appendChild(elPrizeSatsLabel);
 
             // Rows
             var divElement = document.createElement('div');
             divElement.classList.add("score-row");
-            document.getElementById("nameandsats").appendChild(divElement);
+            document.getElementById("highscoresList").appendChild(divElement);
 
             divElement.appendChild(elRank);
-            divElement.appendChild(elP1Name);
-            divElement.appendChild(elP1Sats);
+            divElement.appendChild(winnerDivElement);
             divElement.appendChild(elVSLabel);
-            divElement.appendChild(elP2Name);
-            divElement.appendChild(elP2Sats);
-            divElement.appendChild(elSats);
+            divElement.appendChild(loserdivElement);
+            divElement.appendChild(prizedivElement);
         }
     });
 
