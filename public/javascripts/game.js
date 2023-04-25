1f7919cd7abd00deabec3328770a47414c3ca55d
tantra <tantra@tantra.pt>
2023-04-25 03:59:17 +0100

    working on gamepads

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -38,7 +38,10 @@ let totalPoints = initialScoreDistribution[0] + initialScoreDistribution[1]
 let currentScoreDistribution = [SatsP1,SatsP2];
 let percentageInitialP1 = ((initialScoreDistribution[0] * 100) / totalPoints)/100;
 
-gameSpeed = 1000/10
+let gamepad1 = null;
+let gamepad2 = null;
+
+const gameSpeed = 1000/10
 intervalStart = setInterval(draw, gameSpeed);
 
 let counterStart = 0;
@@ -51,7 +54,7 @@ function counterStartFunc(){
 
 function draw(){
     clear();
-    updateGamepads();
+    listenToGamepads();
     updateScore();
     displayTitle();
     displayGame();
@@ -444,159 +447,6 @@ function checkCollisions(){
     }
 }
 
-addEventListener("keydown", function(event) {
-    switch (event.key.toUpperCase()) {
-        case "ENTER":
-            if(gameStarted==false){
-                countdownStart = true;
-                intervalMain = setInterval(counterStartFunc, 1000);
-            }
-            if(gameEnded==true){
-                let winner = "";
-                if(currentScoreDistribution[0]<=0){
-                    winner="Player 2"
-                }
-                else if(currentScoreDistribution[1]<=0){
-                    winner="Player 1"
-                }
-                sessionStorage.setItem("gameWinner", winner);
-                window.location.href = "/postgame";
-            }
-            break;
-        case "ARROWLEFT":
-            if(p2Dir == "Up" || p2Dir == "Down" || p2Dir == ""){
-                p2DirWanted = "Left"
-            }
-            break;
-        case "ARROWRIGHT":
-            if(p2Dir == "Up" || p2Dir == "Down"){
-                p2DirWanted = "Right"
-            }
-            break;
-        case "ARROWUP":
-            if(p2Dir == "Left" || p2Dir == "Right"){
-                p2DirWanted = "Up"
-            }
-            break;
-        case "ARROWDOWN":
-            if(p2Dir == "Left" || p2Dir == "Right"){
-                p2DirWanted = "Down"
-            }
-            break;
-        case "A":
-            if(p1Dir == "Up" || p1Dir == "Down"){
-                p1DirWanted = "Left"
-            }
-            break;
-        case "D":
-            if(p1Dir == "Up" || p1Dir == "Down" || p1Dir == ""){
-                p1DirWanted = "Right"
-            }
-            break;
-        case "W":
-            if(p1Dir == "Left" || p1Dir == "Right"){
-                p1DirWanted = "Up"
-            }
-            break;
-        case "S":
-            if(p1Dir == "Left" || p1Dir == "Right"){
-                p1DirWanted = "Down"
-            }
-            break;
-    }
-});
-
-// Define the two gamepads
-let gamepad1 = null;
-let gamepad2 = null;
-
-// Get the current gamepad state on each animation frame
-function updateGamepads() {
-  // Check for gamepad connection
-  if (navigator.getGamepads()[0]) {
-    if(gamepad1==null){
-        console.log("Gamepad 1 connected")
-    }
-    gamepad1 = navigator.getGamepads()[0];
-    if(gamepad1.buttons[0].pressed==true || gamepad1.buttons[1].pressed==true || gamepad1.buttons[2].pressed==true || gamepad1.buttons[3].pressed==true){
-        window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'Enter'}));
-    }
-    /*
-    // Code for GAMEPAD
-    if(gamepad1.buttons[12].pressed==true){
-        window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'w'}));
-    }
-    if(gamepad1.buttons[13].pressed==true){
-        window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'s'}));
-    }
-    if(gamepad1.buttons[14].pressed==true){
-        window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'a'}));
-    }
-    if(gamepad1.buttons[15].pressed==true){
-        window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'d'}));
-    }
-    */
-    // CODE FOR ARCADE CONTROLLER
-    //console.log("PLAYER 1")
-    //console.log("0 == "+ gamepad1.axes[0]) // Left = -1 || Right = 1
-    //console.log("1 == "+ gamepad1.axes[1]) // Up = -1 || Down = 1
-    if(gamepad1.axes[1]==-1){
-        window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'w'}));
-    }
-    if(gamepad1.axes[1]==1){
-        window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'s'}));
-    }
-    if(gamepad1.axes[0]==-1){
-        window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'a'}));
-    }
-    if(gamepad1.axes[0]==1){
-        window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'d'}));
-    }
-  }
-  if (navigator.getGamepads()[1]) {
-    if(gamepad2==null){
-        console.log("Gamepad 2 connected")
-    }
-    gamepad2 = navigator.getGamepads()[1];
-    if(gamepad2.buttons[0].pressed==true || gamepad2.buttons[1].pressed==true || gamepad2.buttons[2].pressed==true || gamepad2.buttons[3].pressed==true){
-        window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'Enter'}));
-    }
-    /*
-    // Code for GAMEPAD
-    if(gamepad2.buttons[12].pressed==true){
-        window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'ArrowUp'}));
-    }
-    if(gamepad2.buttons[13].pressed==true){
-        window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'ArrowDown'}));
-    }
-    if(gamepad2.buttons[14].pressed==true){
-        window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'ArrowLeft'}));
-    }
-    if(gamepad2.buttons[15].pressed==true){
-        window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'ArrowRight'}));
-    }
-    */
-    // CODE FOR ARCADE CONTROLLER
-    //console.log("PLAYER 2")
-    //console.log("0 == "+ gamepad2.axes[0]) // Left = -1 || Right = 1
-    //console.log("1 == "+ gamepad2.axes[1]) // Up = -1 || Down = 1
-    if(gamepad2.axes[1]==-1){
-        window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'ArrowUp'}));
-    }
-    if(gamepad2.axes[1]==1){
-        window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'ArrowDown'}));
-    }
-    if(gamepad2.axes[0]==-1){
-        window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'ArrowLeft'}));
-    }
-    if(gamepad2.axes[0]==1){
-        window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'ArrowRight'}));
-    }
-  }
-}
-
-
-
 function displayTitle(){
     let percentageCurrentP1 = ((currentScoreDistribution[0] * 100) / totalPoints)/100;
 
@@ -665,4 +515,159 @@ function displayTitle(){
     ctxTitle.font = "16px Inter";
     ctxTitle.fillStyle = "LightGray";
     ctxTitle.fillText("CURRENT DISTRIBUTION", larguraTitle*0.5, 85);
+}
+
+addEventListener("keydown", function(event) {
+    switch (event.key.toUpperCase()) {
+        case "ENTER":
+            if(gameStarted==false){
+                countdownStart = true;
+                intervalMain = setInterval(counterStartFunc, 1000);
+            }
+            if(gameEnded==true){
+                let winner = "";
+                if(currentScoreDistribution[0]<=0){
+                    winner="Player 2"
+                }
+                else if(currentScoreDistribution[1]<=0){
+                    winner="Player 1"
+                }
+                sessionStorage.setItem("gameWinner", winner);
+                window.location.href = "/postgame";
+            }
+            break;
+        case "ARROWLEFT":
+            if(p2Dir == "Up" || p2Dir == "Down" || p2Dir == ""){
+                p2DirWanted = "Left"
+            }
+            break;
+        case "ARROWRIGHT":
+            if(p2Dir == "Up" || p2Dir == "Down"){
+                p2DirWanted = "Right"
+            }
+            break;
+        case "ARROWUP":
+            if(p2Dir == "Left" || p2Dir == "Right"){
+                p2DirWanted = "Up"
+            }
+            break;
+        case "ARROWDOWN":
+            if(p2Dir == "Left" || p2Dir == "Right"){
+                p2DirWanted = "Down"
+            }
+            break;
+        case "A":
+            if(p1Dir == "Up" || p1Dir == "Down"){
+                p1DirWanted = "Left"
+            }
+            break;
+        case "D":
+            if(p1Dir == "Up" || p1Dir == "Down" || p1Dir == ""){
+                p1DirWanted = "Right"
+            }
+            break;
+        case "W":
+            if(p1Dir == "Left" || p1Dir == "Right"){
+                p1DirWanted = "Up"
+            }
+            break;
+        case "S":
+            if(p1Dir == "Left" || p1Dir == "Right"){
+                p1DirWanted = "Down"
+            }
+            break;
+    }
+});
+
+function listenToGamepads() {
+    if(gamepad1==null){
+        if (navigator.getGamepads()[0]) {
+            console.log("Gamepad 1 connected")
+            gamepad1 = navigator.getGamepads()[0];
+        }
+    }
+    if(gamepad1!=null){
+        gamepad1 = navigator.getGamepads()[0];
+        if(gamepad1.buttons[0].pressed==true){
+            window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'Enter'}));
+        }
+
+        /*
+        // Code for GAMEPAD
+        if(gamepad1.buttons[12].pressed==true){
+            window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'w'}));
+        }
+        if(gamepad1.buttons[13].pressed==true){
+            window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'s'}));
+        }
+        if(gamepad1.buttons[14].pressed==true){
+            window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'a'}));
+        }
+        if(gamepad1.buttons[15].pressed==true){
+            window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'d'}));
+        }
+        */
+
+        // CODE FOR ARCADE CONTROLLER
+        //console.log("PLAYER 1")
+        //console.log("0 == "+ gamepad1.axes[0]) // Left = -1 || Right = 1
+        //console.log("1 == "+ gamepad1.axes[1]) // Up = -1 || Down = 1
+        if(gamepad1.axes[1]<-0.5){
+            window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'w'}));
+        }
+        if(gamepad1.axes[1]>0.5){
+            window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'s'}));
+        }
+        if(gamepad1.axes[0]<-0.5){
+            window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'a'}));
+        }
+        if(gamepad1.axes[0]>0.5){
+            window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'d'}));
+        }
+    }
+    if(gamepad2==null){  
+        if(navigator.getGamepads()[1]) { 
+            console.log("Gamepad 2 connected")
+            gamepad2 = navigator.getGamepads()[1];
+        }  
+    }
+    else if(gamepad2!=null){
+        gamepad2 = navigator.getGamepads()[1];
+        if(gamepad2.buttons[0].pressed==true){
+            window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'Enter'}));
+        }
+
+        /*
+        // Code for GAMEPAD
+        if(gamepad2.buttons[12].pressed==true){
+            window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'ArrowUp'}));
+        }
+        if(gamepad2.buttons[13].pressed==true){
+            window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'ArrowDown'}));
+        }
+        if(gamepad2.buttons[14].pressed==true){
+            window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'ArrowLeft'}));
+        }
+        if(gamepad2.buttons[15].pressed==true){
+            window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'ArrowRight'}));
+        }
+        */
+
+        // CODE FOR ARCADE CONTROLLER
+        //console.log("PLAYER 2")
+        //console.log("0 == "+ gamepad2.axes[0]) // Left = -1 || Right = 1
+        //console.log("1 == "+ gamepad2.axes[1]) // Up = -1 || Down = 1
+        if(gamepad2.axes[1]<-0.5){
+            window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'ArrowUp'}));
+        }
+        if(gamepad2.axes[1]>0.5){
+            window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'ArrowDown'}));
+        }
+        if(gamepad2.axes[0]<-0.5){
+            window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'ArrowLeft'}));
+        }
+        if(gamepad2.axes[0]>0.5){
+            window.dispatchEvent(new KeyboardEvent('keydown',  {'key':'ArrowRight'}));
+        }
+    }
 }
