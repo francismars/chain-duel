dd5a6d73010cf7b50b07da5e61fb9703234166b4
Pedro <pedromvprg@gmail.com>
2023-08-09 21:14:04 +0100

    Test svg for 8 players.

--- public/javascripts/tournbracket.js
+++ public/javascripts/tournbracket.js
@@ -1,4 +1,4 @@
-var players = ["Francis","Pedro","Hal","Nakamoto"];
+var players = ["Francis","Pedro","Hal","Nakamoto","John","Mark","Jamie","Milton"];
 
 
 var a = document.getElementById("bracket4players");
@@ -10,8 +10,9 @@ a.addEventListener("load",function(){
   var svgDoc = a.contentDocument;
 
 
-  /* GAME LOGIC */
+  /* GAME LOGIC 4 */
 
+  /*
   name(svgDoc,"G1_P1", players[0]);
   name(svgDoc,"G1_P2", players[1]);
     highLight(svgDoc,"G1_P1", players[0]);
@@ -23,6 +24,41 @@ a.addEventListener("load",function(){
   name(svgDoc,"G3_P2", players[3]);
       highLight(svgDoc,"G3_P2", players[3]);
       highLight(svgDoc,"Winner", players[3]);
+  */
+
+  /* GAME LOGIC 8 */
+
+  name(svgDoc,"G1_P1", players[0]);
+  name(svgDoc,"G1_P2", players[1]);
+    highLight(svgDoc,"G1_P1", players[0]);
+  name(svgDoc,"G2_P1", players[2]);
+  name(svgDoc,"G2_P2", players[3]);
+    highLight(svgDoc,"G2_P2", players[3]);
+
+    name(svgDoc,"G5_P1", players[0]);
+    name(svgDoc,"G5_P2", players[3]);
+      highLight(svgDoc,"G5_P2", players[3]);
+
+
+  name(svgDoc,"G3_P1", players[4]);
+  name(svgDoc,"G3_P2", players[5]);
+      highLight(svgDoc,"G3_P2", players[5]);
+  name(svgDoc,"G4_P1", players[6]);
+  name(svgDoc,"G4_P2", players[7]);
+    highLight(svgDoc,"G4_P1", players[6]);
+
+    name(svgDoc,"G6_P1", players[5]);
+    name(svgDoc,"G6_P2", players[6]);
+      highLight(svgDoc,"G6_P2", players[6]);
+
+
+      name(svgDoc,"G7_P1", players[3]);
+      name(svgDoc,"G7_P2", players[6]);
+        highLight(svgDoc,"G7_P1", players[3]);
+
+          highLight(svgDoc,"Winner", players[3]);
+
+
 
 }, false);
 
@@ -31,6 +67,7 @@ function highLight(svgDoc,id, name){
   svgDoc.getElementById(id+'_name').textContent = name;
   svgDoc.getElementById(id+'_name').style.fill = "#000";
   svgDoc.getElementById(id+'_rect').style.fill = "#fff";
+  console.log(id);
   svgDoc.getElementById(id+'_path').style.opacity = 1;
   svgDoc.getElementById(id+'_path').style.strokeWidth = 2;
 }
