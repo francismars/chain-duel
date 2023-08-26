d5e4c1fec39737ab1f989623d0310bfc2c8abd0f
tantra <tantra@tantra.pt>
2023-08-26 15:13:45 +0100

    changed buttons logic index

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -24,20 +24,39 @@ addEventListener("keydown", function(event) {
       }
       else if(menu==2){
         menu = 3;
-        document.getElementById("starttourn").style.animationDuration  = "2s";
-        document.getElementById("highscoresbutton").style.animationDuration  = "0s";
+        document.getElementById("starttourn").style.animationDuration  = "0s";
+        document.getElementById("aboutbutton").style.animationDuration  = "2s";
       }
     }
     if (event.key === "ArrowUp" || event.key === "w"){
       if(menu==2){
         menu = 1;
         document.getElementById("startgame").style.animationDuration  = "2s";
-        document.getElementById("aboutbutton").style.animationDuration  = "0s";
+        document.getElementById("starttourn").style.animationDuration  = "0s";
       }
       else if(menu==3){
         menu = 2;
-        document.getElementById("aboutbutton").style.animationDuration  = "2s";
+        document.getElementById("aboutbutton").style.animationDuration  = "0s";
+        document.getElementById("starttourn").style.animationDuration  = "2s";
+      }
+      else if(menu==4){
+        menu = 2;
         document.getElementById("highscoresbutton").style.animationDuration  = "0s";
+        document.getElementById("starttourn").style.animationDuration  = "2s";
+      }
+    }
+    if (event.key === "ArrowRight" || event.key === "d"){
+      if(menu==3){
+        menu = 4;
+        document.getElementById("aboutbutton").style.animationDuration  = "0s";
+        document.getElementById("highscoresbutton").style.animationDuration  = "2s"; 
+      }
+    }
+    if (event.key === "ArrowLeft" || event.key === "a"){
+      if(menu==4){
+        menu = 3;
+        document.getElementById("aboutbutton").style.animationDuration  = "2s";
+        document.getElementById("highscoresbutton").style.animationDuration  = "0s"; 
       }
     }
   });
