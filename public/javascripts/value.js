6b47badab55caad55b251c8998f7603708aae8f2
Pedro <pedromvprg@gmail.com>
2023-10-03 12:36:33 +0100

    Fix check element

--- public/javascripts/value.js
+++ public/javascripts/value.js
@@ -1,6 +1,6 @@
 
 // Global Sponsorship
-if (document.getElementsByClassName("sponsorship").length != 0){
+if (document.getElementsByClassName("sponsorship")){
   const sponsorship = document.getElementsByClassName("sponsorship");
   sponsorship[0].style.display = 'block';
 }
@@ -26,36 +26,36 @@ if (document.getElementById("sponsorship-braket")){
 
 
 
-if (document.getElementsByClassName("sponsored-by-label").length != 0){
+if (document.getElementsByClassName("sponsored-by-label")){
   const sponsoredLabel = document.getElementsByClassName("sponsored-by-label");
   sponsoredLabel[0].innerHTML = "Sponsored by";
   //sponsoredLabel[0].innerHTML = "50,000 sats * 16 BUY IN MATCH <span id='sponsorship-amount'>800,000</span> sats prize donated by";
 }
 
 
-if (document.getElementsByClassName("sponsored-img").length != 0){
+if (document.getElementsByClassName("sponsored-img")){
   const sponsoredImg = document.getElementsByClassName("sponsored-img");
   sponsoredImg[0].src = "/images/sponsors/piratehash.png";
+  sponsoredImg[0].src = "/images/sponsors/bitbox.png";
   sponsoredImg[0].src = "/images/sponsors/relai.png";
-  //sponsoredImg[0].src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fschweizer-minimalist.ch%2Fwp-content%2Fuploads%2F2022%2F07%2FRelai.png&f=1&nofb=1&ipt=6c0ad501df9b8c0d6bdc861722c8382494a4aa521af99704b67f7510fc1274a3&ipo=images";
 }
 
 
 
 
-if (document.getElementById("split1").length != 0){
+if (document.getElementById("split1")){
   const split1 = document.getElementById("split1");
   split1.innerHTML = "<b>2%</b> <span id='hostFee'></span> to the Sponsor (@piratehash)";
 }
 
 
-if (document.getElementById("split2").length != 0){
+if (document.getElementById("split2")){
   const split2 = document.getElementById("split2");
   split2.innerHTML = "<b>2%</b> <span id='devFee'></span> to the developer (@francismars)";
 }
 
 
-if (document.getElementById("split3").length != 0){
+if (document.getElementById("split3")){
   const split3 = document.getElementById("split3");
   split3.innerHTML = "<b>1%</b> <span id='designerFee'></span> to the designer (@bitcoinanatomy)";
 }
