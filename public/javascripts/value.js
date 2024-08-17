2fe44105de16b1611121ea91bd08c598aba615a7
francismars <11526661+francismars@users.noreply.github.com>
2024-08-17 15:22:38 +0100

    organized value file

--- public/javascripts/value.js
+++ public/javascripts/value.js
@@ -1,17 +1,13 @@
+const hideSponsorInfo = false
+const sponsorshipSwitch = false;
 
-let sponsorImage = "";
-
-//sponsorImage = "/images/sponsors/bitcoin_magazine.svg";
-//sponsorImage = "/images/sponsors/piratehash.png";
-//sponsorImage = "/images/sponsors/bitbox.png";
-//sponsorImage = "/images/sponsors/relai_bg.png";
-//sponsorImage = "/images/sponsors/build_the_builders.png";
-//sponsorImage = "/images/sponsors/bitcoin_amsterdam.png";
-sponsorImage = "/images/sponsors/amityage_color.png";
-//sponsorImage = "/images/sponsors/piratehash_animated.png";
+const sponsorText =  "Hosted by" //"50,000 sats * 16 BUY IN MATCH <span id='sponsorship-amount'>800,000</span> sats prize donated by";
+const sponsorName = "@Naomibtc_"
+const imageList = ["bitcoin_magazine.svg", "piratehash.png", "bitbox.png", "relai_bg.png", "build_the_builders.png", 
+                   "bitcoin_amsterdam.png", "amityage_color.png", "piratehash_animated.png"]
+const sponsorImage = "/images/sponsors/"+imageList[6];
 
 // Global Sponsorship
-let sponsorshipSwitch = 1;
 if (document.getElementsByClassName("sponsorship") != null){
   if (document.getElementsByClassName("sponsorship").length != 0){
     const sponsorship = document.getElementsByClassName("sponsorship");
@@ -21,44 +17,40 @@ if (document.getElementsByClassName("sponsorship") != null){
   }
 }
 // Overides
-/*
-if (document.getElementById("sponsorship-index").length != 0){
-  const sponsorshipBraket = document.getElementById("sponsorship-index");
-  sponsorshipBraket.style.display = 'none';
+if(hideSponsorInfo == true){
+  if (document.getElementById("sponsorship-index").length != 0){
+    const sponsorshipBraket = document.getElementById("sponsorship-index");
+    sponsorshipBraket.style.display = 'none';
+  }
+  
+  if (document.getElementById("sponsorship-prefs").length != 0){
+    const sponsorshipBraket = document.getElementById("sponsorship-prefs");
+    sponsorshipBraket.style.display = 'none';
+  }
+  
+  if (document.getElementById("sponsorship-braket").length != 0){
+    const sponsorshipBraket = document.getElementById("sponsorship-braket");
+    sponsorshipBraket.style.display = 'none';
+  }
+  
+  if (document.getElementById("sponsorshipGameMenu").length != 0){
+    const sponsorshipBraket = document.getElementById("sponsorshipGameMenu");
+    sponsorshipBraket.style.display = 'none';
+  }
+  
+  if (document.getElementById("sponsorshipGame").length != 0){
+    const sponsorshipBraket = document.getElementById("sponsorshipGame");
+    sponsorshipBraket.style.display = 'none';
+  }
 }
-
-if (document.getElementById("sponsorship-prefs").length != 0){
-  const sponsorshipBraket = document.getElementById("sponsorship-prefs");
-  sponsorshipBraket.style.display = 'none';
-}
-
-if (document.getElementById("sponsorship-braket").length != 0){
-  const sponsorshipBraket = document.getElementById("sponsorship-braket");
-  sponsorshipBraket.style.display = 'none';
-}
-
-if (document.getElementById("sponsorshipGameMenu").length != 0){
-  const sponsorshipBraket = document.getElementById("sponsorshipGameMenu");
-  sponsorshipBraket.style.display = 'none';
-}
-
-if (document.getElementById("sponsorshipGame").length != 0){
-  const sponsorshipBraket = document.getElementById("sponsorshipGame");
-  sponsorshipBraket.style.display = 'none';
-}
-*/
-
 
 
 if (document.getElementsByClassName("sponsored-by-label") != null){
   if (document.getElementsByClassName("sponsored-by-label").length != 0){
     const sponsoredLabel = document.getElementsByClassName("sponsored-by-label");
-    sponsoredLabel[0].innerHTML = "Hosted by";
-    //sponsoredLabel[0].innerHTML = "50,000 sats * 16 BUY IN MATCH <span id='sponsorship-amount'>800,000</span> sats prize donated by";
+    sponsoredLabel[0].innerHTML = sponsorText;
   }
 }
-
-
 
 
 if (document.getElementsByClassName("sponsored-img") != null){
@@ -66,11 +58,9 @@ if (document.getElementsByClassName("sponsored-img") != null){
     const sponsoredImg = document.getElementsByClassName("sponsored-img");
     sponsoredImg[0].src = sponsorImage;
   }
-  //sponsorImage = "/images/sponsors/stack_sats_relai.png";
   if (document.getElementsByClassName("sponsored-img").length == 2){
     const sponsoredImg = document.getElementsByClassName("sponsored-img");
     sponsoredImg[1].src = sponsorImage;
-    //sponsoredImg[1].style.display = 'none';
   }
 }
 
@@ -78,17 +68,15 @@ if (document.getElementsByClassName("sponsored-img") != null){
 if (document.getElementById("split1") != null){
   if (document.getElementById("split1").length != 0){
     const split1 = document.getElementById("split1");
-    split1.innerHTML = "<b>2%</b> <span id='hostFee'></span> to the host (@AmityAge)";
+    split1.innerHTML = `<b>2%</b> <span id='hostFee'></span> to the host (${sponsorName})`;
   }
 }
-
 if (document.getElementById("split2") != null){
   if (document.getElementById("split2").length != 0){
     const split2 = document.getElementById("split2");
     split2.innerHTML = "<b>2%</b> <span id='developerFee'></span> to the developer (@BTCfrancis)";
   }
 }
-
 if (document.getElementById("split3") != null){
   if (document.getElementById("split3").length != 0){
     const split3 = document.getElementById("split3");
